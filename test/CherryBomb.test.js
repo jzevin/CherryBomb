7af5cfd705f1331d14808f1e1923173308b56a48
Jeremy Zevin <jeremy.zevin@gmail.com>
2020-04-11 18:56:51 -0400

    improving tests

--- test/CherryBomb.test.js
+++ test/CherryBomb.test.js
@@ -1,38 +1,40 @@
 // SUPER NAIVE TESTS
 
 import CherryBomb from "../src/CherryBomb";
-import {expect, assert} from "chai";
-import {JSDOM} from "jsdom";
+import { expect, assert } from "chai";
+import { JSDOM } from "jsdom";
 import canvas from "canvas";
 
-const {window: win} = new JSDOM('<!doctype html><html><body><canvas id="cnv"></canvas></body></html>');
+const { window: win } = new JSDOM(
+  '<!doctype html><html><body><canvas id="cnv"></canvas></body></html>'
+);
 
 global.document = win.document;
 global.window = win.defaultView;
 window = win;
 
-console.log(document.querySelector('canvas'));
-
-
-const
-  canvasELMock = {
-    getContext: () => {
-      return {
-        canvas: { width: 0, height: 0 },
-      };
-    },
-  };
-
-describe('CherryBomb', () => {
-  describe('#production', () => {
-    it('should have production', () => {
-      expect(CherryBomb).to.have.property('production');
-      assert.typeOf(new CherryBomb.production(document.querySelector('canvas')), 'object');
+describe("CherryBomb", () => {
+  let myProduction, myScenes;
+  before(() => {
+    myProduction = new CherryBomb.production(document.querySelector("canvas"));
+    myScenes = [
+      new CherryBomb.scene("scene 1"),
+      new CherryBomb.scene("scene 2"),
+      new CherryBomb.scene("scene 3"),
+    ];
+  });
+  describe("#production", () => {
+    it("should have production", () => {
+      expect(CherryBomb).to.have.property("production");
+      expect(CherryBomb).itself.to.respondTo('production');
+      expect(myProduction).to.be.an.instanceof(CherryBomb.production);
     });
   });
-  describe('#scene', () => {
-    it('should have scene', () => {
-      expect(CherryBomb).to.have.property('scene');
+  describe("#scene", () => {
+    it("should have scene", () => {
+      expect(CherryBomb).to.have.property("scene");
+      expect(CherryBomb).itself.to.respondTo('scene');
+      expect(myScenes[0]).to.be.an.instanceof(CherryBomb.scene);
     });
   });
 });
@@ -43,8 +45,6 @@ describe('CherryBomb', () => {
 // add three scenes to the production
 // make the second one active
 // remove the first
-
-
 
 // describe("CherryBomb is a CherryBomb", () => {
 //   let myProduction, myScenes;
@@ -79,6 +79,5 @@ describe('CherryBomb', () => {
 //       await expect(scene).toBeInstanceOf(CherryBomb.scene);
 //     });
 //   });
-
 
 // });
